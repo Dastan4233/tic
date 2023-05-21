e008a6f705211fbdbf007abfb4736e163b9f5ee9
Dastan4233 <134033166+Dastan4233@users.noreply.github.com>
2023-05-22 02:41:29 +1300

    tic-toe

---
+++ tic-toe-tok/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className='square'>
+                {/* {Todo} */}
+            </button>
+        )
+    }
+}
+
+class Board extends React.Component {
+    renderSquare
+}
+
+export default App
